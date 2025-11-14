3b243dab7baf26fbb7bd4088aa3e235f31a7dab1
Steve Rozario <srozar20@montgomerycollege.edu>
2025-11-14 13:23:21 -0500

    Added function for card validation

--- collaborative_assignment_card_game_inst326.py
+++ collaborative_assignment_card_game_inst326.py
@@ -1,3 +1,44 @@
+
+#Steve Rozario
+def card_validation(hand, played_card, current_suit):
+    """Returns the valid card using Crazy 8 and Uno rules. It handles concerns
+    determining whether or not the cards played are valid/unvalid.
+
+    Args:
+        hand (list[Card]): The hand player has currently.
+        played_card (Card): The recent card on top of the card pile
+        current_suit (str): To follow the suit that is being played unless it 
+        is changed by the wild card(8).
+
+    Returns:
+        Card or None: Plays the first card that is valid, otherwise it will 
+        return none.
+    """
+    valid_cards= []
+    for card in hand:
+        #rule 1: Makes sure the suit matches
+        if card.suit == current_suit:
+            valid_cards.append(card)
+            
+        #rule 2: The ranks of the cards match 
+        elif card.ranktype == played_card.ranktype:
+            valid_cards.append(card)
+        
+        #rule 3: cards that follow uno rules(special)
+        elif card.action is not None:
+            valid_cards.append(card)
+            
+        #rule 4: Crazy 8/wildcard
+        elif card.ranktype == "8":
+            valid_cards.append(card)
+    #Draw cards because no valid cards on hand
+    if not valid_cards:
+        return None
+    
+    #return card that will be valid
+    return valid_cards[0]
+
+#Brandon
 def apply_special_action(card, get_next_player, draw_cards, reverse_direction):
     """Applies a special card action (skip, reverse, draw 2). Uses mock
     functions for the purpose of deliverable."""
